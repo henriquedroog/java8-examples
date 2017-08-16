504651d18a5ae58e3d50d9206b2093b604396c2e
Henrique Droog <henrique.droog@blackwelllearning.co.uk>
2017-08-16 15:51:20 +0100

    First commit

---
+++ java8-streams/src/main/java/uk/co/droog/java8/streams/App.java
@@ -0,0 +1,13 @@
+package uk.co.droog.java8.streams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
