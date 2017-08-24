44373e7e956b831c2325d4f083d39bc72cd34828
Henrique Droog <henrique.droog@blackwelllearning.co.uk>
2017-08-24 20:44:22 +0100

    Fixing maven setup

--- java8-streams/src/test/java/uk/co/droog/java8/streams/AppTest.java
+++ java8-streams/src/test/java/uk/co/droog/java8/streams/AppTest.java
@@ -1,38 +1,37 @@
 package uk.co.droog.java8.streams;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	@Test
+	public void streamFilterTest() {
+		List<String> myList = Arrays.asList("a1","a2","b1","b2","c1","c2");
+		
+		List<String> result = myList
+			.stream()
+			.filter(s -> s.startsWith("c"))
+			.map(String::toUpperCase)
+			.sorted()
+			.collect(Collectors.toList());
+		
+		assertThat(result, contains("C1","C2"));
+		assertThat(result, not(contains("c1","c2")));
+	}
 }
