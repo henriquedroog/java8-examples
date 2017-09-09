75efdcfc1eafd57b06e208b23e9b3b00e88c4a6f
Henrique Droog <henrique.droog@blackwelllearning.co.uk>
2017-09-09 14:17:41 +0100

    Adding some more stream tests

--- java8-streams/src/test/java/uk/co/droog/java8/streams/AppTest.java
+++ java8-streams/src/test/java/uk/co/droog/java8/streams/AppTest.java
@@ -1,5 +1,8 @@
 package uk.co.droog.java8.streams;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
@@ -7,6 +10,8 @@ import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
@@ -14,6 +19,70 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest {
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testStreamFromArrays(){
+		System.out.println("AppTest.testStreamFromArrays");
+		Arrays.asList("a1","a2","a3")
+			.stream()
+			.findFirst()
+			.ifPresent(System.out::println);
+		
+	}
+	
+	@Test
+	public void testStreamFromArrays_mapToString(){
+		System.out.println("AppTest.testStreamFromArrays_mapToString");
+		String result = Arrays.asList("a1","a2","a3")
+			.stream()
+			.findFirst()
+			.get();
+		assertThat(result, is("a1"));
+	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testStreamOf(){
+		System.out.println("AppTest.testStreamOf");
+		List<String> resultList = Stream.of("a1","a2","a3")
+			.collect(Collectors.toList());
+		assertThat(resultList, hasItem("a1"));
+		assertThat(resultList, hasItem("a2"));
+		assertThat(resultList, hasItem("a3"));
+	}
+	
+	@Test
+	public void testIntStream_forEach(){
+		System.out.println("AppTest.testIntStream_forEach");
+		IntStream.range(1, 4)
+			.forEach(System.out::println);
+	}
+	
+	@Test
+	public void testIntStream_mapOperation(){
+		System.out.println("AppTest.testIntStream_mapOperation");
+		IntStream.range(1, 4)
+		.map(n -> 2* n + 1)
+		.forEach(System.out::println);
+	}
+	
+	@Test
+	public void testIntStream_mapOperationAverageToIt(){
+		System.out.println("AppTest.testIntStream_mapOperationAverageToIt");
+		Double result = IntStream.range(1, 4)
+		.map(n -> 2* n + 1)
+		.average()
+		.getAsDouble();
+		 
+		assertThat(result, equalTo(5.0D));
+
+	}
+	
 	/**
 	 * Create the test case
 	 *
@@ -21,7 +90,8 @@ public class AppTest {
 	 *            name of the test case
 	 */
 	@Test
-	public void streamFilterTest() {
+	public void testStreamFilter_StartsWith() {
+		System.out.println("AppTest.testStreamFilter_StartsWith");
 		List<String> myList = Arrays.asList("a1","a2","b1","b2","c1","c2");
 		
 		List<String> result = myList
---
+++ java8-streams/src/test/java/uk/co/droog/java8/streams/SimpleDTO.java
@@ -0,0 +1,31 @@
+package uk.co.droog.java8.streams;
+
+public class SimpleDTO {
+	
+	private Integer id;
+	private String title;
+	public SimpleDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public SimpleDTO(Integer id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+
+}
